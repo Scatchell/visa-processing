0f51011692a00d3a900f043845dd238b175c72f8
Anthony Scatchell <Scatchell@gmail.com>
2013-12-06 14:27:15 +0300

    Created rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VisaProcessing::Application.config.secret_key_base = '1f7e763f4a0906aaccdea64c1418e392b57f85f7a4290adec01412d3a9f7e5398999c254d79831d7c532bb370fa263a3050665139f73e28acf275b4fde554f52'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VisaProcessing::Application.config.session_store :cookie_store, key: '_visaProcessing_session'
